086ee327eb468c35d9ecdd8cab9f99082ef9dd6d
Dan Lopuch <dan@datahero.com>
2014-12-08 00:28:39 -0800

    Adding lightstrip init code

--- glow-ops.js
+++ glow-ops.js
@@ -1,2 +1,13 @@
-console.log("Starting glowstrip!");
-require('./lib/controllers/glowstrip');
+
+console.log('Initializing lightstrip controller...');
+require('./controllers/lightstrip').open(function(error, results) {
+  if (error) {
+    console.log('ERROR: Could not initiate lightstrip controller: ', error);
+    return process.exit(1);
+  }
+
+  console.log("Lightstrip engaged!");
+  console.log("Starting glowstrip!");
+
+  require('./lib/controllers/glowstrip');
+});
